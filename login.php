acc5c54098745c7531d8a80a37180e3c774f9a8a
Tomas Gonzalez <tpg3@njit.edu>
2024-09-30 12:38:31 -0400

    asd

--- login.php
+++ login.php
@@ -1,27 +1,21 @@
 <?php
-header('Content-Type: application/json'); // Set the content type to JSON
+header('Content-Type: application/json');
 
-// Check if the POST request is empty
-if (empty($_POST)) {
-    $response = ["message" => "No POST message set, politely fuck off"];
-    echo json_encode($response); // Send the JSON response
-    exit(0);
+if (!isset($_POST))
+{
+	$msg = "NO POST MESSAGE SET, POLITELY FUCK OFF";
+	echo json_encode($msg);
+	exit(0);
 }
-
 $request = $_POST;
-$response = ["message" => "Unsupported request type, politely FUCK OFF"]; // Default response
-
-// Check the type of request
-if (isset($request["type"])) {
-    switch ($request["type"]) {
-        case "login":
-            // Here, we can assume login logic or success response
-            $response = ["message" => "Login successful!"]; // Example response for a successful login
-            break;
-    }
+$response = "unsupported request type, politely FUCK OFF";
+switch ($request["type"])
+{
+	case "login":
+		$response = "login, yeah we can do that";
+	break;
 }
-
-// Return the response as JSON
-echo json_encode($response); // Encode and send the response
+echo json_encode($response);
 exit(0);
+
 ?>
