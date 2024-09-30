9b40bf32b1e98ac3c2f05f7a03c4cf21edb000d2
Tomas Gonzalez <tpg3@njit.edu>
2024-09-30 12:36:51 -0400

    asdas

--- login.php
+++ login.php
@@ -1,26 +1,27 @@
 <?php
-header('Content-Type: application/json');
+header('Content-Type: application/json'); // Set the content type to JSON
 
 // Check if the POST request is empty
 if (empty($_POST)) {
     $response = ["message" => "No POST message set, politely fuck off"];
-    echo json_encode($response);
+    echo json_encode($response); // Send the JSON response
     exit(0);
 }
 
 $request = $_POST;
-$response = ["message" => "Unsupported request type, politely FUCK OFF"]; // Use an associative array for the response
+$response = ["message" => "Unsupported request type, politely FUCK OFF"]; // Default response
 
 // Check the type of request
 if (isset($request["type"])) {
     switch ($request["type"]) {
         case "login":
-            $response = ["message" => "Login, yeah we can do that"]; // Set the response as an associative array
+            // Here, we can assume login logic or success response
+            $response = ["message" => "Login successful!"]; // Example response for a successful login
             break;
     }
 }
 
 // Return the response as JSON
-echo json_encode($response);
+echo json_encode($response); // Encode and send the response
 exit(0);
 ?>
