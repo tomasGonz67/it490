8db3baea58cb341163a4bdd4d66ff9c2bd218c43
Tomas Gonzalez <tpg3@njit.edu>
2024-10-07 21:02:53 -0400

    testing

--- testRabbitMQServer.php
+++ testRabbitMQServer.php
@@ -7,6 +7,7 @@ require_once('rabbitMQLib.inc');
 function doLogin($username,$password)
 {
     require_once 'mysqlconnect.php';
+    echo "hello?";
     echo checkLogin($username, $password);
     //return false if not valid
 }
