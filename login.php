534f9f940e4d0e62882d611b35629a37273377b7
Tomas Gonzalez <tpg3@njit.edu>
2024-10-07 20:18:18 -0400

    test

--- login.php
+++ login.php
@@ -2,7 +2,7 @@
 require_once('path.inc');
 require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
-
+header('Content-Type: application/json');
 if (!isset($_POST))
 {
 	$msg = "NO POST MESSAGE SET, POLITELY FUCK OFF";
