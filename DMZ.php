f31eb6a57f7dccb8dc0ff73f3f7e62fbdce6c91f
Luke da Silva <lad5@njit.edu>
2024-11-04 13:21:12 -0500

    dmz work

--- DMZ.php
+++ DMZ.php
@@ -1,8 +1,10 @@
 #!/usr/bin/php
 <?php
+require_once('rabbitMQLib.inc');
+function requestProcessor($req) {}
 function getFighters(){
 			$curl = curl_init();
-            $client = new rabbitMQClient("testRabbitMQ.ini","DMZ");
+	                $client = new rabbitMQClient("testRabbitMQ.ini","DMZ");
 			curl_setopt($curl, CURLOPT_URL, "https://ufc-api-theta.vercel.app/mma-api/fighters");
 			curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 
@@ -16,4 +18,11 @@ function getFighters(){
             ];
             return $response = $client->send_request($request);
         }
+
+$server = new rabbitMQServer("testRabbitMQ.ini", "DMZ");
+
+echo "DMZ SERVER BEGIN".PHP_EOL;
+$server->process_requests('getFighters');
+echo "DMZ SERVER END".PHP_EOL;
+
 ?>
